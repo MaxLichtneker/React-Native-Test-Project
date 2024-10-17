e9d127ba3d84301416bee98f2d04912b0c253d69
MaxLichtneker <maxwil2001@hotmail.com>
2024-10-17 10:18:22 +0200

    more updates

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TouchableHighlight, SafeAreaView,Image} from 'react-native';
+import { StyleSheet, Text, View,TouchableHighlight, SafeAreaView,Image, Button, Alert,Platform,StatusBar, Dimensions} from 'react-native';
 
 export default function App() {
 
   const handlePress = () => console.log("text pressed");
-  
+  const containerStyle = {backgroundColor: "orange"};
+
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1}>Hello world!</Text>
 
-      <TouchableHighlight onPress={() => console.log("Pressed image")}>
-        <Image loadingIndicatorSource={require("./assets/favicon.png")} source={{
-          width:200,
-          height: 300,
-          uri: "https://picsum.photos/200/300"}}/>
-      </TouchableHighlight>
+      <SafeAreaView>
+        <TouchableHighlight onPress={() => console.log("Pressed image")}>
+          <Image loadingIndicatorSource={require("./assets/favicon.png")} source={{
+            width:200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"}}/>
+        </TouchableHighlight>
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.container}>
+      <Button color="orange" title='Click me' onPress={() => console.log("Button Tapped")}/>
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.container}>
+        <Button title='Click me' 
+        onPress={() => Alert.alert("My title","My Message",[{text: "Yes"}, {text: "No"},])}/>  
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.container}>
+        <view
+          style={{
+            backgroundColor: "dodgerblue",
+            width: "50%",
+            height:"70",
+          }}
+        />
+      </SafeAreaView>
 
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -25,8 +46,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: "center"
   },
 });
 
